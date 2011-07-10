fbdd23bdac36deb43266c0a5747e90f0c7b1e8ca
wata <w.nagasawa@gmail.com>
2011-07-10 16:49:53 +0900

    initial commit

---
+++ lib/Amon2/Setup/Flavor/Wata.pm
@@ -0,0 +1,32 @@
+package Amon2::Setup::Flavor::Wata;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Amon2::Setup::Flavor::Wata -
+
+=head1 SYNOPSIS
+
+  use Amon2::Setup::Flavor::Wata;
+
+=head1 DESCRIPTION
+
+Amon2::Setup::Flavor::Wata is
+
+=head1 AUTHOR
+
+Wataru Nagasawa E<lt>nagasawa {at} junkapp.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
